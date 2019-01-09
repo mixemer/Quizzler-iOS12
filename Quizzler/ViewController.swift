af1f47d080dbc44a409b9c46684e31c20d35db0a
mixemer <mehmetshin96@gmail.com>
2019-01-08 22:47:30 -0500

    Create Quizzler app and implement its functionalities

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var questionNumber = 0
+    var selectedAnswer = false
+    var score = 0
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -20,32 +24,70 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        
+        if sender.tag == 1 {
+            selectedAnswer = true
+        }
+        else if sender.tag == 2 {
+            selectedAnswer = false
+        }
+        checkAnswer()
+        
+        questionNumber += 1
+        nextQuestion()
+        scoreLabel.text = "Score: \(score)"
     }
     
     
     func updateUI() {
-      
+        progressLabel.text = "\(questionNumber + 1)/13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
 
     func nextQuestion() {
         
-    }
-    
-    
-    func checkAnswer() {
+        if questionNumber < 13 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            updateUI()
+        }
+        else {
+            // show user that user has reach to the end. Should user start over?
+            let alert = UIAlertController(title: "Awesome", message: "You have reach to the end of questions. Do you want to start over?", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(alertAction)
+            self.present(alert, animated: true, completion: nil)
+        }
         
     }
     
     
+    func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if selectedAnswer == correctAnswer {
+            score += 1
+            ProgressHUD.showSuccess("Correct!")
+        }
+        else {
+            ProgressHUD.showError("Wrong!")
+        }
+    }
+    
+    
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        nextQuestion()
+        updateUI()
     }
     
 
